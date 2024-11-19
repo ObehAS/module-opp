226f1cfb26e2d3d3e62ac5e13db5ba079ce6360a
Andrey Sazonov <sazonov2a@mail.ru>
2024-11-19 21:06:20 +0500

    Work 4

---
+++ OPP2.4.py
@@ -0,0 +1,104 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.lecturer_grades:
+                lecturer.lecturer_grades[course].append(grade)
+            else:
+                lecturer.lecturer_grades[course] = [grade]
+        else:
+            print(f"Студент {self.name} {self.surname} не может оценить лектора {lecturer.name} {lecturer.surname} по курсу '{course}'.")
+
+    def get_average_grade(self):
+        if not self.grades:
+            return 0
+        total_grades = sum(sum(grades) for grades in self.grades.values())
+        total_count = sum(len(grades) for grades in self.grades.values())
+        return total_grades / total_count if total_count > 0 else 0
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания: {self.get_average_grade():.1f}\n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {', '.join(self.finished_courses)}")
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.get_average_grade() < other.get_average_grade()
+        return NotImplemented
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lecturer_grades = {}
+
+    def get_average_grade(self):
+        if not self.lecturer_grades:
+            return 0
+        total_grades = sum(sum(grades) for grades in self.lecturer_grades.values())
+        total_count = sum(len(grades) for grades in self.lecturer_grades.values())
+        return total_grades / total_count if total_count > 0 else 0
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {self.get_average_grade():.1f}")
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.get_average_grade() < other.get_average_grade()
+        return NotImplemented
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            print(f"Эксперт {self.name} {self.surname} не может выставить оценку студенту {student.name} {student.surname} за курс '{course}'.")
+
+    def __str__(self):
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"У лекторов:")
+
+
+# Подсчета средней оценки за домашние
+def average_student_grade(students, course):
+    total_grades = 0
+    total_count = 0
+    for student in students:
+        if course in student.grades:
+            total_grades += sum(student.grades[course])
+            total_count += len(student.grades[course])
+    return total_grades / total_count if total_count > 0 else 0
+
+
+# Подсчета средней оценки за лекции
+def average_lecturer_grade(lecturers, course):
+    total_grades = 0
+    total_count = 0
+    for lecturer in lecturers:
+        if course in lecturer.lecturer_grades:
+            total_grades += sum(lecturer.lecturer_grades[course])
+            total_count += len(lecturer.lecturer_grades[course])
+    return total_grades / total_count if total_count > 0 else 0
